0a9393856d2aaf5ac8377b5490887069e2637593
Raj <uxthemer@gmail.com>
2025-03-13 18:10:01 +0530

    crackers categories image compression and package name changes

--- src/components/CrackerCategories.tsx
+++ src/components/CrackerCategories.tsx
@@ -29,14 +29,13 @@ export function CrackerCategories() {
         </div>
         <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4 md:gap-6">
           {categories.map((category) => (
-            <Link to={`/explore?category=${category.name.toLowerCase()}`} className="transform hover:scale-110 transition-transform duration-500">
+            <Link key={category.id} to={`/explore?category=${category.name.toLowerCase()}`} className="transform hover:scale-110 transition-transform duration-500">
               <div
-                key={category.id}
                 className="card group relative overflow-hidden rounded-xl shadow-lg p-2 flex flex-col items-center text-center"
               >
                 <div className="w-16 h-16 md:w-20 md:h-20 flex items-center justify-center">
                   <img
-                    src={category.image_url ? `/assets/img/crackers/${category.image_url}` : `/assets/img/logo/logo_2.png`}
+                    src={category.image_url ? `/assets/img/categories/${category.image_url}` : `/assets/img/logo/logo_2.png`}
                     alt={category.name}
                     className="w-full h-full object-contain transition-transform duration-500"
                   />
